c320f3fd0eadc28f48ad08c83367b22cb7473415
ajones424 <ajones061392@cogswebdevelopmentandhosting.com>
2023-03-16 17:52:58 -0700

    Another install script.

---
+++ scripts/installation_scripts/utilities/i3wm/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#   Description: The purpose of this script is to keep track of
+#                the packages that are installed to modify and 
+#                maintain my current i3wm config file. 
+#   List of Packages: py3status conky 
+
+#Make an array of the packages 
+
+#Installing packages
+echo "Installing packages"
+sudo pacman -S py3status conky
+
